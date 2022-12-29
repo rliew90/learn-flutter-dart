2102e46bd9d9d920c14f1c7e876e316688281ddc
mw <notalk_0@hotmail.com>
2022-12-29 22:49:48 +0800

    initial commit

---
+++ lib/answer.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final Function selectHandler;
+
+  Answer(this.selectHandler);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.blue),
+          foregroundColor: MaterialStateProperty.all(Colors.white),
+        ),
+        child: Text('Answer 1'),
+        onPressed: selectHandler,
+      ),
+    );
+  }
+}
